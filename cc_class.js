b12bac19fa6c1828d08a037b007216c48a806dfd
Riley Hedges <rhedges1@usf.edu>
2025-02-11 16:33:39 -0500

    Task 3

--- cc_class.js
+++ cc_class.js
@@ -25,3 +25,12 @@ console.log(applyDIscount(products, .1))
 console.log("*".repeat(20))
 console.log(products)
 
+// Task 3 
+
+let sales = [250, 400, 150, 900, 1200];
+
+function calculateTotalRevenue(sales) {
+    return sales.reduce((total, sale) => total + sale, 0)
+}
+
+console.log(`Total Revenue 2024: $${calculateTotalRevenue(sales)}`)
